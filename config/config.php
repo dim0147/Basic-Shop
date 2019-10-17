24f4ad514435d6c76af2f4fe13a5eb28c4f05569
dim0147 <provandam0369@gmail.com>
2019-10-17 17:12:57 +0800

    ADD DEVELOP PATH ENVIRONMENT

--- config/config.php
+++ config/config.php
@@ -3,7 +3,9 @@
     define('DB_NAME', 'shop');
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
-    
+    define('DEVELOP_FOLDER', 'MVCProject');
+	
+	
     $LOAD_CORE = [
         'blade_template',
         'controller',
--- core/route.php
+++ core/route.php
@@ -3,7 +3,7 @@
 
     function route($url, Closure $callback){
         global $routes;
-        $url = trim('/WEBASSIGNMENT2' . $url, '/');
+        $url = trim(DEVELOP_FOLDER . $url, '/');
         $routes[$url] = $callback;
     }
 
