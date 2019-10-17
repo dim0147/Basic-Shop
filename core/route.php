980acb571abfa68da377e9dfa9c10d1ff8fee777
dim0147 <provandam0369@gmail.com>
2019-10-17 23:13:35 +0800

    fix cf route

--- core/route.php
+++ core/route.php
@@ -3,15 +3,7 @@
 
     function route($url, Closure $callback){
         global $routes;
-<<<<<<< HEAD
-<<<<<<< HEAD
-        $url = trim(DEVELOP_PATH . $url, '/');
-=======
         $url = trim(DEVELOP_FOLDER . $url, '/');
->>>>>>> d37d107912cf9e947efc5cdc395478113beb2b7a
-=======
-        $url = trim(DEVELOP_FOLDER . $url, '/');
->>>>>>> 24f4ad514435d6c76af2f4fe13a5eb28c4f05569
         $routes[$url] = $callback;
         
     }
